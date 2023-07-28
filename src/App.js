909c8e9c97ee3c8178d2c1a01f048fc661eab386
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-28 14:53:37 +0300

    installed react iconspackage-lock.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import BotCollection from "./components/BotCollection";
 
 function App() {
-  return <div className="App"></div>;
+  const [robots, setRobots] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch("http://localhost:3000/bots")
+        .then((r) => r.json())
+        .then((bots) => setRobots(bots));
+    } catch (error) {
+      console.log(error, "something went wrong");
+    }
+  }, []);
+
+  return (
+    <div className="app">
+      <BotCollection renderBots={robots} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/BotCollection.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { GiTemplarShield, GiCrossedSwords } from "react-icons/gi";
+import { BsHeartPulseFill } from "react-icons/bs";
+import "./BotCollection.css";
+
+function BotCollection({ renderBots }) {
+  const botCards = (bots) => {
+    return bots.map((bot) => {
+      return (
+        <article key={bot.id} className="botCard">
+          <div className="botAvatar">
+            <img src={bot.avatar_url} alt="bot_image" />
+          </div>
+          <div className="botBody">
+            <div className="botInfo">
+              <h3 className="botName">{bot.name}</h3>
+              <p className="botCatchPhrase">{bot.catchphrase}</p>
+            </div>
+            <div className="botStats">
+              <ul className="stats">
+                <li className="stat">
+                  <BsHeartPulseFill />
+                  {bot.health}
+                </li>
+                <li className="stat">
+                  <GiCrossedSwords />
+                  {bot.damage}
+                </li>
+                <li className="stat">
+                  <GiTemplarShield />
+                  {bot.armor}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </article>
+      );
+    });
+  };
+
+  return <div className="robotsCollection">{botCards(renderBots)}</div>;
+}
+
+export default BotCollection;
