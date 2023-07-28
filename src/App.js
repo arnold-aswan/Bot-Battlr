8412051c39121417cd48e0e7c118906d045b0a5b
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-28 17:03:42 +0300

    rendered BotCollection

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import BotCollection from "./components/BotCollection";
+import YourBotCollection from "./components/YourBotCollection";
+// import { GiTemplarShield, GiCrossedSwords } from "react-icons/gi";
+// import { BsHeartPulseFill } from "react-icons/bs";
 
 function App() {
   const [robots, setRobots] = useState([]);
+  const [yourBots, setYourBots] = useState([]);
 
   useEffect(() => {
     try {
@@ -16,9 +20,64 @@ function App() {
     }
   }, []);
 
+  const handleEnlist = (bot) => {
+    console.log(bot);
+    const army = robots.filter((bots) => bots.id === bot);
+    setYourBots([...yourBots, army]);
+    // console.log(yourBots);
+  };
+
+  // const botCards = (bots) => {
+  //   return bots.map((bot) => {
+  //     return (
+  //       <article
+  //         key={bot.id}
+  //         className="botCard"
+  //         onClick={() => handleEnlist(bot.id)}
+  //       >
+  //         <div className="botAvatar">
+  //           <img src={bot.avatar_url} alt="bot_image" />
+  //         </div>
+  //         <div className="botBody">
+  //           <div className="botInfo">
+  //             <h3 className="botName">{bot.name}</h3>
+  //             <p className="botCatchPhrase">{bot.catchphrase}</p>
+  //           </div>
+  //           <div className="botStats">
+  //             <ul className="stats">
+  //               <li className="stat">
+  //                 <BsHeartPulseFill />
+  //                 {bot.health}
+  //               </li>
+  //               <li className="stat">
+  //                 <GiCrossedSwords />
+  //                 {bot.damage}
+  //               </li>
+  //               <li className="stat">
+  //                 <GiTemplarShield />
+  //                 {bot.armor}
+  //               </li>
+  //             </ul>
+  //           </div>
+  //         </div>
+  //       </article>
+  //     );
+  //   });
+  // };
+
+  // const handleClick = (bot) => {
+  //   enlist(bot);
+  // };
+
   return (
     <div className="app">
-      <BotCollection renderBots={robots} />
+      <YourBotCollection
+        enlisted={yourBots}
+        shield={GiTemplarShield}
+        attack={GiCrossedSwords}
+        health={BsHeartPulseFill}
+      />
+      <BotCollection renderBots={robots} enlist={handleEnlist} />
     </div>
   );
 }
