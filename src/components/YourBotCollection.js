cab2bfd5acc6a0e4b23ec4fbcdd29673a309d7b7
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-29 12:28:24 +0300

    added filter function to yourBotCollection component

--- src/components/YourBotCollection.js
+++ src/components/YourBotCollection.js
@@ -1,38 +1,54 @@
 import React from "react";
 import "./YourBotCollection.css";
 
-function YourBotCollection({ enlisted, shield, attack, health }) {
-  console.log(enlisted);
+function YourBotCollection({ enlisted, shield, attack, health, discharge }) {
+  const handleDischarge = (botId) => {
+    discharge(botId);
+  };
 
   function yourBots(enlistedBots) {
-    return enlistedBots.map((bot) => {
+    // initialize empty object
+    const botsById = {};
+    // filter based on bot id
+    const filteredBots = enlistedBots.filter((bot) => {
+      // if bot id ! in botsById obj add it & return true
+      if (!botsById[bot.id]) {
+        botsById[bot.id] = true;
+        return true;
+      } else {
+        // else if bot id is already in botsById obj return false
+        return false;
+      }
+    });
+    // map with the filtered obj
+    return filteredBots.map((bot) => {
       return (
         <article
-          key={bot[0].id}
+          key={bot.id}
           className="botCard"
-          //   onClick={() => handleEnlist(bot.id)}
+          onClick={() => handleDischarge(bot.id)}
         >
           <div className="botAvatar">
-            <img src={bot[0].avatar_url} alt="bot_image" />
+            <img src={bot.avatar_url} alt="bot_image" />
           </div>
           <div className="botBody">
             <div className="botInfo">
-              <h3 className="botName">{bot[0].name}</h3>
-              <p className="botCatchPhrase">{bot[0].catchphrase}</p>
+              <h3 className="botName">{bot.name}</h3>
+              <p className="botCatchPhrase">{bot.catchphrase}</p>
             </div>
             <div className="botStats">
               <ul className="stats">
                 <li className="stat">
                   {health}
-                  {bot[0].health}
+                  {bot.health}
                 </li>
                 <li className="stat">
                   {attack}
-                  {bot[0].damage}
+                  {bot.damage}
                 </li>
                 <li className="stat">
                   {shield}
-                  {bot[0].armor}
+                  {bot.armor}
                 </li>
               </ul>
             </div>
